266de9896134c84dcd28b77f88f9fe6153e2bffe
corxworks <corxworks@gmail.com>
2026-08-12 14:30:58 +0530

    Connect frontend to production backend

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:5000";
+  "https://manager-x.onrender.com";
 
 
 // =========================================================
